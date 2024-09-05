78a79bfec4c3ca8b264bc4271c82139bdeb4286f
JaviSabor <javierignaciosabor@gmail.com>
2024-09-04 23:48:13 -0300

    se agrega el boton de reiniciar en el contador

--- app/contador.tsx
+++ app/contador.tsx
@@ -1,27 +1,46 @@
-import { View, Text, Button } from 'react-native'
-import React, { useState } from 'react'
+import { Button, Text, View } from "react-native";
+import React, { useState } from "react";
 
-type Props = {}
-
-const Contador = (props: Props) => {
-
-    const [contador, setContador] = useState(0)
-
-    const incrementar = () => {
-        setContador(prev => prev + 1)
-    }
+function Contador() {
+    const [contador, setContador] = useState(0);
 
     const decrementar = () => {
-        setContador(prev => prev - 1)
+        setContador(prev => prev - 1);
+    }
+    const incrementar = () => {
+        setContador(prev => prev + 1);
     }
 
+    const reset = () => {
+        setContador(0);
+    }
+
+    //Generame otra funcion con otro boton que al clickear se muestre un numero random
+    const random = () => {
+        setContador(Math.floor(Math.random() * 100));
+    }
+    <View style={{ marginBottom: 24 }}>
+        <Button onPress={random} title="Random" />
+    </View>
+
+
+
+
+
     return (
-        <View>
-            <Text>Contador: {contador}</Text>
-            <Button title='Incrementar' onPress={incrementar} />
-            <Button title='Decrementar' onPress={decrementar} />
+        <View style={{ marginBottom: 24 }}>
+            <Text style={{ marginBottom: 24 }}> Contador actual: {contador}</Text>
+            <View style={{ marginBottom: 24 }}>
+                <Button onPress={incrementar} title="Incrementar" />
+            </View>
+            <View style={{ marginBottom: 24 }}>
+                <Button onPress={decrementar} title="Decrementar" />
+            </View>
+            <View style={{ marginBottom: 24 }}>
+                <Button onPress={reset} title="Reiniciar" />
+            </View>
         </View>
-    )
+    );
 }
 
-export default Contador
+export default Contador;
