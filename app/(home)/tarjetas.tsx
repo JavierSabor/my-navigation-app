04f52f64f62d1da7e87e93d73c80891ae5adeed6
JaviSabor <javierignaciosabor@gmail.com>
2024-09-10 16:00:01 -0300

    Refactor componente Tarjetas. Diseñado para agregar tarjetas, estilo centrados

--- app/(home)/tarjetas.tsx
+++ app/(home)/tarjetas.tsx
@@ -13,27 +13,37 @@ const Tarjeta = ({ texto }: TarjetaProps) => {
   };
 
   return (
-  <Pressable style={styles.Contenedor} onPress={handlePress}>
+    <View >
+  <Pressable  style={styles.Contenedor} onPress={handlePress}>
 	  <Text style = {[styles.Card, isTouched && styles.CardTouched]}>
 		{texto}
 	  </Text>
 	</Pressable>
+    </View>
+
   
   );
 };
 
 const ListaTarjetas = () => {
   return (
-    <>
-      <Tarjeta texto="Tarjeta 1: VISA BANCO PATAGONIA" />
-      <Tarjeta texto="Tarjeta 2: MASTERCARD BANCO GALICIA" />
-      <Tarjeta texto="Tarjeta 3: AMEX BANCO SANTANDER" />
-      <Tarjeta texto="Tarjeta 4: AMEX BANCO AMERICAN EXPRESS" />
-    </>
+<View style={styles.Lista}>
+    <Tarjeta texto="Tarjeta 1: VISA BANCO PATAGONIA" />
+    <Tarjeta texto="Tarjeta 2: MASTERCARD BANCO GALICIA" />
+    <Tarjeta texto="Tarjeta 3: AMEX BANCO SANTANDER" />
+    <Tarjeta texto="Tarjeta 4: AMEX BANCO AMERICAN EXPRESS" />
+    <Tarjeta texto="Tarjeta 5: VISA BANCO NACION" />
+</View>
   );
 };
 
 const styles = StyleSheet.create({
+
+  Lista: {
+    flex: 1,
+    height: '100%',
+    justifyContent: 'center',
+  },
 
 Contenedor:{
     flex: 1,
@@ -42,23 +52,25 @@ Contenedor:{
     flexDirection: 'column',
     marginBottom  : 40,
     marginTop: 80,
-    },
-
-Card: {
-    color: 'white',
-    backgroundColor: '#5b9bd5',
-    textAlign: 'center',
-    fontSize: 18,
-    padding: "auto",
-    width: "80%",
-    height: 'auto',
-    minHeight: 105,
-    borderWidth: 3,
-    borderColor: "black",
-    borderRadius: 30,
-
+    marginHorizontal: 30,
     
+
     },
+
+    Card: {
+      color: 'white',
+      backgroundColor: '#5b9bd5',
+      textAlign: 'center',
+      fontSize: 18,
+      padding: 30, 
+      height: 'auto',
+      minHeight: 105,
+      borderWidth: 3,
+      borderColor: "black",
+      borderRadius: 30,
+      width: "100%",
+    },
+
 CardTouched: {
     color: 'white',
     backgroundColor: '#333',
